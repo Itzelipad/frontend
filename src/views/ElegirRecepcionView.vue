78bbc5363e4d9ef6da0c4886b5bede3f8340fbf4
HDJ-Vega <147786880+HDJ-Vega@users.noreply.github.com>
2024-11-27 00:13:23 -0600

    Seleccion de recepcion en usuario recepcionista

--- src/views/ElegirRecepcionView.vue
+++ src/views/ElegirRecepcionView.vue
@@ -6,15 +6,14 @@
     </div>
 
     <div class="mb-6 ml-4 mr-4">
-      <p class="text-[#163891] text-lg md:text-lg"> Ingrese a la recepción que desea para ver sus estadísticas e ingresar datos.</p>
+      <p class="text-[#163891] text-lg md:text-lg"> Ingrese a la recepción que desea para ver sus estadísticas e
+        ingresar datos.</p>
     </div>
 
     <!-- Lista de recepciones -->
-    <div v-for="(recepcion, index) in recepciones"
-      :key="index"
+    <div v-for="(recepcion, index) in recepciones" :key="index"
       class="bg-white p-4 rounded-xl mb-4 flex justify-between items-center"
-      style="box-shadow: 0px 4px 10px rgba(112, 144, 176, 0.12)"
-    >
+      style="box-shadow: 0px 4px 10px rgba(112, 144, 176, 0.12)">
       <!-- Texto de la recepción -->
       <div>
         <div class="text-[#163891] font-semibold text-xl"> Recepción {{ recepcion.numero }} </div>
@@ -22,31 +21,86 @@
       </div>
 
       <!-- Botón de flecha para redirigir al formulario -->
-      <button @click="$router.push('/formulario')" class="focus:outline-none">
-        <svg class="w-6 h-6 text-[#163891] transition-transform duration-200 hover:scale-110" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"  fill="none"  viewBox="0 0 24 24">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="m9 5 7 7-7 7"/>
+      <button @click="seleccionRecepcion(recepcion)" class="focus:outline-none">
+        <svg class="w-6 h-6 text-[#163891] transition-transform duration-200 hover:scale-110" aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3"
+            d="m9 5 7 7-7 7" />
         </svg>
       </button>
     </div>
 
     <!-- Botón de cerrar sesión -->
     <div class="flex justify-end mt-8">
-      <button class="bg-[#2B3674] text-[#FFFFFF] px-6 py-2 rounded-lg transition font-semibold"> Cerrar sesión </button>
+      <button @click="cerrarSesion" class="bg-[#2B3674] text-[#FFFFFF] px-6 py-2 rounded-lg transition font-semibold">
+        Cerrar sesión </button>
     </div>
   </div>
 </template>
 
 <script>
+import methods from '@/methods';
+import axios from 'axios';
 export default {
   data() {
     return {
-      recepciones: [
-        { numero: 503, edificio: "A" },
-        { numero: 504, edificio: "B" },
-        { numero: 505, edificio: "C" },
-        { numero: 506, edificio: "D" },
-      ],
+      recepciones: [],
     };
   },
+  methods: {
+    cerrarSesion() {
+      let token = localStorage.getItem('token');
+      axios
+        .delete(`${this.$apiRoute}/logout`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        })
+        .then(response => {
+          methods.deleteProperties(this.$user);
+          alert(response.data.message);
+          this.$router.push('/');
+        })
+        .catch(error => {
+          if (error.status === 401)
+            alert("error al cerrar sesion.");
+          console.error("error al cerrar sesion", error);
+        });
+    },
+    seleccionRecepcion(recepcion) {
+      let token = localStorage.getItem('token');
+      axios
+        .patch(`${this.$apiRoute}/seleccionar-recepcion/${recepcion.id}`,{},
+          {
+            headers: {
+              'Authorization': `Bearer ${token}`
+            }
+          })
+        .then(response => {
+          alert(response.data.message);
+          this.$user.reception = recepcion.id;
+          this.$router.push('/formulario');
+        })
+        .catch(error => {
+          console.error('Error al realizar la peticion:', error);
+        });
+      
+    }
+  },
+  mounted() {
+    let token = localStorage.getItem('token');
+    axios
+      .get(`${this.$apiRoute}/recepciones-disponibles`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      .then(response => {
+        this.recepciones = response.data.recepciones;
+      })
+      .catch(error => {
+        console.error('Error al realizar la peticion:', error);
+      });
+  }
 };
 </script>
