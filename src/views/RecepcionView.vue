03fc97ebce8ed16abd26468a5ad60c3987d4b73d
HDJ-Vega <147786880+HDJ-Vega@users.noreply.github.com>
2024-11-28 13:56:37 -0600

    Cambio para estadisticas de recepcionista

--- src/views/RecepcionView.vue
+++ src/views/RecepcionView.vue
@@ -6,11 +6,14 @@
     <!-- Contenido principal de la página -->
     <div class="p-4 flex-1">
       <div class="flex justify-between items-center mt-4 md:mt-6 mb-4">
-        <h1 class="text-2xl md:text-3xl font-semibold ml-0 md:ml-12" style="color: #163891">Recepción {{ receptionNumber }}</h1>
+        <h1 class="text-2xl md:text-3xl font-semibold ml-0 md:ml-12" style="color: #163891">Recepción {{ receptionNumber
+          }}</h1>
         <!-- Botón Descargar -->
         <button class="flex items-center text-[#163891] px-4 focus:outline-none mr-14 hidden md:flex">
-          <svg class="w-6 h-6 text-[#163891] mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 15v2a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-2m-8 1V4m0 12-4-4m4 4 4-4"/>
+          <svg class="w-6 h-6 text-[#163891] mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+            height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M4 15v2a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-2m-8 1V4m0 12-4-4m4 4 4-4" />
           </svg>
           <span class="font-semibold">Descargar</span>
         </button>
@@ -22,19 +25,18 @@
       <div class="mt-4 flex flex-col md:flex-row">
         <!-- Gráfica de barras -->
         <div class="ml-0 md:ml-11">
-          <BarChart 
-            :chartLabels="['C', 'R', 'I', 'E', 'S']" 
-            :chartDataValues="chartDataValues" 
-            @update-chart="updateChartData"
-          />
+          <BarChart :chartLabels="['C', 'R', 'I', 'E', 'S']" :chartDataValues="chartDataValues"
+            @update-chart="updateChartData" />
         </div>
 
         <!-- Gráfica de anillo -->
-        <div class="mt-4 md:mt-0 md:ml-10"> <DoughnutChart /></div>
-      </div> 
+        <div class="mt-4 md:mt-0 md:ml-10">
+          <DoughnutChart />
+        </div>
+      </div>
 
-       <!-- Componente de estadísticas de doctores-->
-      <EstadisticasDocComp/>
+      <!-- Componente de estadísticas de doctores-->
+      <EstadisticasDocComp />
     </div>
   </div>
 </template>
@@ -67,7 +69,7 @@ export default {
   },
   computed: {
     receptionNumber() {
-      return this.$auxiliar.numero; 
+      return this.$auxiliar.numero;
     }
   },
   methods: {
@@ -75,6 +77,10 @@ export default {
       this.chartDataValues = this.timeData[selectedOption];
     },
   },
+  beforeCreate() {
+    if (this.$user.reception !== null)
+        this.$auxiliar.id = this.$user.reception;
+  }
 };
 </script>
 
